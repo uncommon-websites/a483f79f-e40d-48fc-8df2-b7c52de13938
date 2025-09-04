78b03a36245675f1e1a3298ca09403b429bb1ff8
Uncle Common <krishnerkar@gmail.com>
2025-09-04 23:07:49 +0000

    Remove background image from hero

--- src/lib/components/layout/hero-sections/CenteredTextOnlyHero.svelte
+++ src/lib/components/layout/hero-sections/CenteredTextOnlyHero.svelte
@@ -36,12 +36,7 @@ Takes up the full viewport height and centers content vertically.
 
 <div class="grid h-[calc(100vh-var(--nav-height))] grid-rows-[1fr_auto]" {...rest}>
 	<div class="bg-background relative isolate flex items-center justify-center">
-		<div
-			style:background-image="url('{imageSrc}')"
-			class="absolute top-0 right-4 bottom-4 left-4 -z-10 rounded-(--radius-xl) bg-cover bg-center brightness-100 after:absolute after:inset-0 after:rounded-(--radius-xl) after:shadow-[inset_0_0_100px_40px_rgba(0,0,0,0.3)]"
-		></div>
-
-		<div class="bg-background/50 absolute inset-0"></div>
+		<!-- Background image removed -->
 
 		<header
 			class="section-px z-50 container mx-auto grid place-items-center text-center text-balance dark:text-white"
